260e82c715ae6fbe6c196a7dc33616a0a932a95c
Sricharan Sriram <scharan97@gmail.com>
2023-10-12 23:20:53 +0800

    Modified code for height.py

--- height.py
+++ height.py
@@ -3,13 +3,12 @@ for _ in range(P):
     inp = list(map(int,input().split()))
     k,lst = inp[0],inp[1:]
     ans = 0
-    for i in range(1,20):
+    for i in range(1,len(lst)):
         x = lst[i]
         j = i-1
-        while j>=0:
-            if lst[j] >= x:
-                lst[j+1] = lst[j]
-                ans += 1
+        while j>=0 and x < lst[j]:
+            lst[j+1] = lst[j]
+            ans += 1
             j-=1
         lst[j+1] = x
     print(k, ans)
